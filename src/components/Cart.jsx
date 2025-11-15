dcff7bcc59f2b6f074a20378b442d39e3533f2ad
user <oprenber@gmail.com>
2025-11-15 18:25:29 +0900

    add

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,25 +3,36 @@ import React, { useState } from 'react';
 const Cart = ({ cart, setCart }) => {
     const [isOpen, setIsOpen] = useState(true);
 
-    //TODO: 장바구니를 열고 닫는 함수를 완성하세요.
-    //setIsOpen 함수를 사용하여 State값을 업데이트하세요.
-    const toggleCart = () => {};
+    const toggleCart = () => {
+        setIsOpen(!isOpen);
+    };
 
-    //TODO: cart에 담긴 상품의 총 가격을 계산하세요.
-    //reduce 함수를 사용하여 총 가격을 계산하세요.
-    const totalPrice = 0;
+    let totalPrice = 0;
+    cart.forEach((item) => {
+        totalPrice += item.price;
+    });
 
-    //TODO: (선택 과제) 상품 가격에 서식을 지정하세요.
-    //상품 가격을 3자리마다 콤마(,)를 찍어주는 함수를 작성하세요.
-    //예시) 1000000 => 1,000,000
-    //이 함수의 사용법은 totalPriceFormatted(가격) 입니다.
-    //HINT : https://shape-coding.tistory.com/72
-    const totalPriceFormatted = () => {};
+    const totalPriceFormatted = (price) => {
+    let priceString = price.toString();
+    let result = "";
+    let count = 0;
 
-    //TODO: 결제하기 버튼 구현하기
-    //결제하기 버튼을 눌렀을 때 alert를 띄우고
-    //장바구니를 비워주세요.
-    const handlePurchase = () => {};
+   for (let i = priceString.length - 1; i >= 0; i--) {
+            result = priceString[i] + result;
+            count++;
+            if (count === 3 && i !== 0) {
+                result = "," + result;
+                count = 0;
+            }
+        }
+
+        return result;
+    };
+
+    const handlePurchase = () => {
+        alert("결제가 완료되었습니다!"); 
+        setCart([]); 
+    };
 
     return (
         <div className="cart">
